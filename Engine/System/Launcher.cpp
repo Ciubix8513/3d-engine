1996a7f632024add438f6a7652e1d75827647c64
Ciubix8513 <Ciubix8513@yandex.ru>
2021-03-28 02:03:31 +0300

    Cleaned up a bit

--- Engine/System/Launcher.cpp
+++ Engine/System/Launcher.cpp
@@ -8,15 +8,7 @@ using namespace Engine;
 
 int main()
 {
-	/*Scene scene;
-	scene.AddEntity("A");
-	scene.AddEntity("B");	
-
-	auto Entity0 = scene.GetEntityByUUID(1);
-	Entity0->SetTag("123");
-	auto Entity1 = scene.GetEntityByTag("123");
-	auto Entity2 = scene.GetEntityByUUID(2);*/
-
+	
 	
 
 	std::cout << ' ';
